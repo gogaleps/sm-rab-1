76d89296ed5ecd6e3a1c41ad9b786734bc17d3c2
gogaleps <91685861+gogaleps@users.noreply.github.com>
2021-10-05 17:13:46 +0300

    george denisov п43021
    george denisov п43021
    george denisov п43021
    george denisov п43021
    george denisov п43021
    george denisov п43021
    george denisov п43021

---
+++ см раб 1 для pablo.cpp
@@ -0,0 +1,12 @@
+/* 1 сторона 8.5 ,2 сторона 10 , 3 сторона 24 */ #include <iostream>
+using namespace std;
+int main()
+{
+	int a, b,c;
+	double s;
+	cout << "naiti";
+	cin >> a >> b >> c;
+		s = a * b * c;
+		cout << s << endl;
+		return 0;
+}
